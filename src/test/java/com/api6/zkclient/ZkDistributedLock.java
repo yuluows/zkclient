f93b1b62df0e6ee335bfb498512ff834c2acf2cc
zhoajie <zh_jie@163.com>
2016-05-31 17:35:00 +0800

    修改测试代码

--- src/test/java/com/api6/zkclient/ZkDistributedLock.java
+++ src/test/java/com/api6/zkclient/ZkDistributedLock.java
@@ -34,89 +34,90 @@ import com.api6.zkclient.util.TestUtil;
 import com.api6.zkclient.util.ZKServer;
 
 public class ZkDistributedLock {
-	
-	private TestSystem testSystem = TestSystem.getInstance();
-	private ZKServer zkServer = null;
-	private ZKClient zkClient = null;
+    
+    private TestSystem testSystem = TestSystem.getInstance();
+    private ZKServer zkServer = null;
+    private ZKClient zkClient = null;
     @Before
     public void before() {
-    	zkServer = testSystem.getZKserver();
-    	zkClient = ZKClientBuilder.newZKClient()
-    							.servers("localhost:"+zkServer.getPort())
-    							.sessionTimeout(1000)
-    							.build();
+        zkServer = testSystem.getZKserver();
+        zkClient = ZKClientBuilder.newZKClient()
+                                .servers("localhost:"+zkServer.getPort())
+                                .sessionTimeout(1000)
+                                .build();
     }
     
     @After
     public void after(){
-    	testSystem.cleanup(zkClient);
+        testSystem.cleanup(zkClient);
     }
    
+    /**
+     * 测试分布式锁
+     * @throws Exception 
+     * @return void
+     */
     @Test
     public void testDistributedLock() throws Exception{
-    	
-    	final String lockPath = "/zk/lock";
-    	zkClient.createRecursive(lockPath, null, CreateMode.PERSISTENT);
-    	final AtomicInteger integer = new AtomicInteger(0);
-    	final List<String> msgList = new ArrayList<String>();
-    	for(int i=0;i<20;i++){
-    		Thread thread1 = new Thread(new Runnable() {
-    			public void run() {
-    				try {
-    					ZKDistributedLock lock = new ZKDistributedLock(zkClient, "/zk/lock");
-        				lock.lock(0);
-        				integer.getAndIncrement();
-        				msgList.add("thread "+integer);
-        				System.out.println("Thread "+integer+" got lock........");
-        				
-        				if(integer.get()==3){
-        					Thread.sleep(1000);
-        				}
-        				if(integer.get()==5){
-        					Thread.sleep(700);
-        				}
-        				
-        				if(integer.get()==6 || integer.get()==11){
-        					Thread.sleep(500);
-        				}
-        				
-        				if(integer.get()==10){
-        					Thread.sleep(500);
-        				}
-        				if(integer.get()==15){
-        					Thread.sleep(400);
-        				}
-        				lock.unlock();
-        				System.out.println("thread "+integer+" unlock........");
-        				
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} finally {
-						
-					}
-    			}
-    		});
-        	thread1.start();
-    	}
-    	
-    	//等待事件到达
-    	int size = TestUtil.waitUntil(20, new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				return msgList.size();
-			}
-    		
-		}, TimeUnit.SECONDS, 100);
-    	assertThat(size).isEqualTo(20);
-    	boolean flag = true;
-    	for(int i =0;i<20;i++){
-			if(!msgList.get(i).equals("thread "+(i+1))){
-				flag = false;
-			}
-    	}
-    	assertThat(flag).isTrue();
-    	
+        final String lockPath = "/zk/lock";
+        zkClient.createRecursive(lockPath, null, CreateMode.PERSISTENT);
+        final AtomicInteger integer = new AtomicInteger(0);
+        final List<String> msgList = new ArrayList<String>();
+        for(int i=0;i<20;i++){
+            Thread thread1 = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        ZKDistributedLock lock = new ZKDistributedLock(zkClient, "/zk/lock");
+                        lock.lock(0);
+                        integer.getAndIncrement();
+                        msgList.add("thread "+integer);
+                        System.out.println("Thread "+integer+" got lock........");
+                        
+                        if(integer.get()==3){
+                            Thread.sleep(1000);
+                        }
+                        if(integer.get()==5){
+                            Thread.sleep(700);
+                        }
+                        
+                        if(integer.get()==6 || integer.get()==11){
+                            Thread.sleep(500);
+                        }
+                        
+                        if(integer.get()==10){
+                            Thread.sleep(500);
+                        }
+                        if(integer.get()==15){
+                            Thread.sleep(400);
+                        }
+                        lock.unlock();
+                        System.out.println("thread "+integer+" unlock........");
+                        
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        
+                    }
+                }
+            });
+            thread1.start();
+        }
+
+        //等待事件到达
+        int size = TestUtil.waitUntil(20, new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return msgList.size();
+            }
+            
+        }, TimeUnit.SECONDS, 100);
+        assertThat(size).isEqualTo(20);
+        boolean flag = true;
+        for(int i =0;i<20;i++){
+            if(!msgList.get(i).equals("thread "+(i+1))){
+                flag = false;
+            }
+        }
+        assertThat(flag).isTrue();
     }
-    
-   
 }
